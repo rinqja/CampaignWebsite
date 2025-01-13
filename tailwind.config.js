68d6afe3ebf8e7955844f6ad12d0960cc0f4983c
rinqja <rkelmendi@jacobs-university.de>
2025-01-13 16:40:04 +0100

    pushed these stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary-blue': '#0056b3',
-        'accent-red': '#e60000',
-        'neutral-white': '#ffffff',
-        'supportive-gold': '#ffcc00',
-        'fresh-green': '#009933',
-        'dark-charcoal': '#333333', 
+        "primary-blue": "#0056b3",
+        "accent-red": "#e60000",
+        "neutral-white": "#ffffff",
+        "supportive-gold": "#ffcc00",
+        "fresh-green": "#009933",
+        "dark-charcoal": "#333333",
       },
       fontFamily: {
-        heading: ['Lato', 'sans-serif'],
-        body: ['Roboto', 'sans-serif'],
+        sans: ["Inter", "sans-serif"],
       },
       fontSize: {
-        h1: ['36px', '44px'], // [font size, line height]
-        h2: ['30px', '38px'],
-        h3: ['24px', '32px'],
-        body: ['16px', '24px'],
-        caption: ['14px', '20px'],
+        h1: ["36px", "44px"], // [font size, line height]
+        h2: ["30px", "38px"],
+        h3: ["24px", "32px"],
+        body: ["16px", "24px"],
+        caption: ["14px", "20px"],
       },
     },
   },
   plugins: [],
-}
+};
